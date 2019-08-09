6468c0aee395fc597707c542e97fdaf723c6d48b
Filipe Duarte <filipe_pb_duarte@hotmail.com>
2019-08-09 16:40:51 -0300

    Curso 2

---
+++ estatistica_descritiva_fun.R
@@ -0,0 +1,15 @@
+# Função estatística descritiva
+
+estatisticas <- function(x){
+  x <- x[!is.na(x)] # isso serve para que ele omita as observações com NA
+  me <- mean(x)
+  med <- median(x)
+  n <- length(x)
+  s <- sd(x)
+  mi <- min(x)
+  ma <- max(x)
+  q25 <- quantile(x, probs = 0.25)
+  q75 <- quantile(x, probs = 0.75)
+  return(c(n = n, media = me, mediana = med, desvio = s, 
+           Q = q25, Q = q75, min = mi, max = ma))
+}
